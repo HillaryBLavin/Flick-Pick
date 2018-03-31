494da89d2d337402f526612f875deb8192c9f4e7
Hillary Lavin <hillarylavin@Hillarys-MacBook-Pro.local>
2018-03-31 11:00:58 -0400

    Delete additional app.js file, add searchAPILKey to original app.js file

--- app.js
+++
@@ -1,9 +0,0 @@
-var searchAPILKey = "api_key=2429acb131d788573608b3142e21e670", //key provided by The Movie Databse API
-    queryURL = 'https://api.themoviedb.org/3/movie/550?' + searchAPILKey
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-});
