a092df5f198a00d945c3b91dd5f5d9def9df5832
Hillary Lavin <hillarylavin@Hillarys-MacBook-Pro.local>
2018-03-31 20:31:39 -0400

    Add pseudocode to bottom of app.js, starting at line 38

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -35,4 +35,43 @@ firebase.initializeApp(config);
 
 
 
-//
+// Flick-Pick JavaScript Pseudocode
+//-------- Basic Logic -----------|
+// 1. Present User with choices
+// 2. Store User's choices in Firebase
+// 3. Retrieve User's choices from Firebase
+// 4. Send API query based on User's choices
+// 5. Return results from API query to display in the DOM
+
+// -------- Expanded Logic -------|
+// Initialize Firebase
+// Create global variables, as needed, for example...
+    //Movie Database API Key and Query URL's
+    // Search Term Query Variables 
+// 1. Present User with choices
+// AND
+// 2. Store Usesr's choices in Firebase (add more choices as needed)
+    // Create on-click event to "Start App"
+    // Create on-click event to submit 1st choice (How much time does the user have?)
+        // Assign data value from user chocie to a query parameter for API
+        // Push user choice to Firebase    
+    // Create on-click event to submit 2nd choice (...)
+        // Assign data value from user chocie to a query parameter for API
+        // Push user choice to Firebase
+    // Create on-click event to submit 3rd choice (...)
+        // Assign data value from user chocie to a query parameter for API
+        // Push user choice to Firebase
+    // Create on-click event to submit 3rd choice (...)
+        // Assign data value from user chocie to a query parameter for API
+        // Push user choice to Firebase
+// 3. Retrieve User's choices from Firebase
+// AND
+// 4. Send API query based on User's choices
+    // Get user choices from Firebase
+    // Concatenate query parameters to API query
+    //AJAX Query Call
+// 5. Return results from API query to display in the DOM
+    // Hook into contentDiv
+    // Use jQuery to create DOM elements for API query results 
+        // <img> tag for poster
+        // <h> or <p> tag(s) for title, synopsis, etc. 
