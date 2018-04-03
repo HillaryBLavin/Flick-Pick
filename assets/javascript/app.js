14195420ea260a9e223041b7195e27b0f4c3bade
Hillary Lavin <hilavin@gmail.com>
2018-04-03 13:29:07 -0400

    Remove pseudocode outline from app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -173,33 +173,3 @@ movieData.ref().on("child_added", function (childSnapshot, prevChildKey) {
         });
     }
 });
-// Create global variables, as needed, for example...
-//Movie Database API Key and Query URL's
-// Search Term Query Variables 
-// 1. Present User with choices
-// AND
-// 2. Store Usesr's choices in Firebase (add more choices as needed)
-// Create on-click event to "Start App"
-// Create on-click event to submit 1st choice (How much time does the user have?)
-// Assign data value from user chocie to a query parameter for API
-// Push user choice to Firebase    
-// Create on-click event to submit 2nd choice (...)
-// Assign data value from user chocie to a query parameter for API
-// Push user choice to Firebase
-// Create on-click event to submit 3rd choice (...)
-// Assign data value from user chocie to a query parameter for API
-// Push user choice to Firebase
-// Create on-click event to submit 3rd choice (...)
-// Assign data value from user chocie to a query parameter for API
-// Push user choice to Firebase
-// 3. Retrieve User's choices from Firebase
-// AND
-// 4. Send API query based on User's choices
-// Get user choices from Firebase
-// Concatenate query parameters to API query
-//AJAX Query Call
-// 5. Return results from API query to display in the DOM
-// Hook into contentDiv
-// Use jQuery to create DOM elements for API query results 
-// <img> tag for poster
-// <h> or <p> tag(s) for title, synopsis, etc.
