ee004aaf8e37bef7aff84ef87674dec327fb5855
Reinaldo Llano, Jr <llanoreinaldo@gmail.com>
2018-04-06 10:25:05 -0400

    add missing brackets to fix scope

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -209,145 +209,144 @@ function tvQuery() {
                 }
             })
         });
+    })
+};
 
-        // MOVIE LOGIC
-        // On-click even for "Movie"
-        $(document.body).on("click", "#choice2", function () {
-            // Empty the contents of the question and answer list divs
-            $("#question").empty();
-            $("#answerList").empty();
-            movieRatingsChoice();
-        })
-        // Define movieRatingsChoice - this will display Movie Ratings options
-        function movieRatingsChoice() {
-            // Display first question in movieQuestions
-            $("#question").html("<h4>" + movieQuestions[0].question + "</h4>");
-            // Display choices using for-loop
-            for (i = 0; i < movieQuestions[0].options.length; i++) {
-                $("#answerList").append("<button class='waves-effect waves-light btn-large' id='movie-rating-choice' data-value='" + movieQuestions[0].valuesID[i] + "'>" + movieQuestions[0].options[i] + "</button>");
-            }
-        }
-        // Create on-click event for when user selets a rating
-        $(document.body).on("click", "#movie-rating-choice", function () {
-            userRating = $(this).data("value");
-            console.log(userRating);
-            $("#question").empty();
-            $("#answerList").empty();
-            movieGenreChoice();
-        })
-        // Define movieGenreChoice - this will display Movie genre options
-        function movieGenreChoice() {
-            // Display second question in movieQuestions
-            $("#question").html("<h4>" + tvQuestions[1].question + "</h4>");
-            // Display choices using for-loop
-            for (i = 0; i < movieQuestions[1].options.length; i++) {
-                $("#answerList").append("<button class='waves-effect waves-light btn-large' id='movie-genre-choice' data-value='" + movieQuestions[1].valuesID[i] + "'>" + movieQuestions[1].options[i] + "</button>");
-            }
-        }
-        // Create on-click event for when user selets a genre
-        $(document.body).on("click", "#movie-genre-choice", function () {
-            userGenre = $(this).data("value");
-            console.log(userGenre);
-            $("#question").empty();
-            $("#answerList").empty();
-            movieQuery();
-        })
-
-
-
-        // Define movieQuery - the ajax call to The Movie Database API
-        function movieQuery() {
-            movieQueryURL = 'https://api.themoviedb.org/3/discover/movie' + apiKey + language + sort + certCountry + cert + userRating + '&include_adult=false' + video + '&page=1&with_genres=' + userGenre;
-            $.ajax({
-                url: movieQueryURL,
-                method: "GET"
-            }).then(function (response) {
-                console.log(response);
-                // Insert code for writing the results to the DOM here
-                finalRecDiv = $("<div>"); // Create a new div
-                finalRecDiv.html("<h4>How about one of these?</h4>"); // Add text
-                clickPoster = $("<div>"); // Create a new div
-                clickPoster.html("<p>Click the poster for more info!</p>"); // Add text
-                if (response.results.length > 0) {
-                    // Create a new div
-                    newDiv = $("<div>");
-                    // Add .carousel (per Materialize Carousel documentation)
-                    newDiv.addClass("carousel carousel-slider center");
-                    for (i = 0; i < 3; i++) {
-                        // This will handle the situation if response has no title
-                        var title = response.results[i].title;
-                        var overview = response.results[i].overview;
-                        // Create an image tag for each of the first three results from the ajax call
-                        var img = $('<img>');
-                        // Add the poster as the src
-                        img.attr("src", 'https://image.tmdb.org/t/p/w185_and_h278_bestv2' + response.results[i].poster_path)
-                        // Add .carousel-item (per Materialize Carousel documentation)
-                        img.attr("class", 'carousel-item');
-                        // Add id for use in on-click event 
-                        img.attr("id", "movie-" + (i + 1));
-                        // Add data-value for title
-                        img.data("title", title);
-                        //Add data-value for overview
-                        img.data("synopsis", overview);
-                        // Append image to the new div
-                        img.appendTo(newDiv);
-                        // Append new divs to #recommendation
-                        finalRecDiv.appendTo('#recommendation');
-                        newDiv.appendTo('#recommendation');
-                        clickPoster.appendTo('#recommendation');
-                        // Store Movie Title and Synopsis to global variables to display on-click
-                        movieTitle.push(title);
-                        movieSynopsis.push(overview);
-                    }
-
-                    $('.carousel').carousel();
-                }
-
-                $(document.body).on("click", "#movie-1", function () {
-                    $("#recommendation").empty();
-                    // Hook into contentDiv
-                    // Adds title and overview to DOM 
-                    $("#recommendation").html("<h4>" + movieTitle[0] + "</h4><p>" + movieSynopsis[0] + "</p>")
-                });
-                $(document.body).on("click", "#movie-2", function () {
-                    $("#recommendation").empty();
-                    // Hook into contentDiv
-                    // Adds title and overview to DOM 
-                    $("#recommendation").html("<h4>" + movieTitle[1] + "</h4><p>" + movieSynopsis[1] + "</p>")
-                });
-                $(document.body).on("click", "#movie-3", function () {
-                    $("#recommendation").empty();
-                    // Hook into contentDiv
-                    // Adds title and overview to DOM 
-                    $("#recommendation").html("<h4>" + movieTitle[2] + "</h4><p>" + movieSynopsis[2] + "</p>")
-                });
-                resetGlobals();
-
-                $("#recommendation").on('click', function () {
-                    $.getJSON("https://itunes.apple.com/search?term=" + tvTitle[i], function (result) {
-                        if (result.results.length > 0) { //Runs if array returns a result
-                            if (result.results[i].kind === "feature-movie") {
-                                //displays results in the DOM 
-                                $('body').append(result.results[i].previewUrl + '<br>')
-
-                            }
-                        }
-                    })
-                });
-            });
-
-
-        };
-
-
-        // Reset global variables
-        function resetGlobals() {
-            userRating = '',
-                userGenre = '',
-                tvQueryURL = '',
-                movieQueryURL = '';
-
-
-        };
-    });
+// MOVIE LOGIC
+// On-click even for "Movie"
+$(document.body).on("click", "#choice2", function () {
+    // Empty the contents of the question and answer list divs
+    $("#question").empty();
+    $("#answerList").empty();
+    movieRatingsChoice();
+})
+// Define movieRatingsChoice - this will display Movie Ratings options
+function movieRatingsChoice() {
+    // Display first question in movieQuestions
+    $("#question").html("<h4>" + movieQuestions[0].question + "</h4>");
+    // Display choices using for-loop
+    for (i = 0; i < movieQuestions[0].options.length; i++) {
+        $("#answerList").append("<button class='waves-effect waves-light btn-large' id='movie-rating-choice' data-value='" + movieQuestions[0].valuesID[i] + "'>" + movieQuestions[0].options[i] + "</button>");
+    }
 }
+// Create on-click event for when user selets a rating
+$(document.body).on("click", "#movie-rating-choice", function () {
+    userRating = $(this).data("value");
+    console.log(userRating);
+    $("#question").empty();
+    $("#answerList").empty();
+    movieGenreChoice();
+})
+// Define movieGenreChoice - this will display Movie genre options
+function movieGenreChoice() {
+    // Display second question in movieQuestions
+    $("#question").html("<h4>" + tvQuestions[1].question + "</h4>");
+    // Display choices using for-loop
+    for (i = 0; i < movieQuestions[1].options.length; i++) {
+        $("#answerList").append("<button class='waves-effect waves-light btn-large' id='movie-genre-choice' data-value='" + movieQuestions[1].valuesID[i] + "'>" + movieQuestions[1].options[i] + "</button>");
+    }
+}
+// Create on-click event for when user selets a genre
+$(document.body).on("click", "#movie-genre-choice", function () {
+    userGenre = $(this).data("value");
+    console.log(userGenre);
+    $("#question").empty();
+    $("#answerList").empty();
+    movieQuery();
+})
+
+
+
+// Define movieQuery - the ajax call to The Movie Database API
+function movieQuery() {
+    movieQueryURL = 'https://api.themoviedb.org/3/discover/movie' + apiKey + language + sort + certCountry + cert + userRating + '&include_adult=false' + video + '&page=1&with_genres=' + userGenre;
+    $.ajax({
+        url: movieQueryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        // Insert code for writing the results to the DOM here
+        finalRecDiv = $("<div>"); // Create a new div
+        finalRecDiv.html("<h4>How about one of these?</h4>"); // Add text
+        clickPoster = $("<div>"); // Create a new div
+        clickPoster.html("<p>Click the poster for more info!</p>"); // Add text
+        if (response.results.length > 0) {
+            // Create a new div
+            newDiv = $("<div>");
+            // Add .carousel (per Materialize Carousel documentation)
+            newDiv.addClass("carousel carousel-slider center");
+            for (i = 0; i < 3; i++) {
+                // This will handle the situation if response has no title
+                var title = response.results[i].title;
+                var overview = response.results[i].overview;
+                // Create an image tag for each of the first three results from the ajax call
+                var img = $('<img>');
+                // Add the poster as the src
+                img.attr("src", 'https://image.tmdb.org/t/p/w185_and_h278_bestv2' + response.results[i].poster_path)
+                // Add .carousel-item (per Materialize Carousel documentation)
+                img.attr("class", 'carousel-item');
+                // Add id for use in on-click event 
+                img.attr("id", "movie-" + (i + 1));
+                // Add data-value for title
+                img.data("title", title);
+                //Add data-value for overview
+                img.data("synopsis", overview);
+                // Append image to the new div
+                img.appendTo(newDiv);
+                // Append new divs to #recommendation
+                finalRecDiv.appendTo('#recommendation');
+                newDiv.appendTo('#recommendation');
+                clickPoster.appendTo('#recommendation');
+                // Store Movie Title and Synopsis to global variables to display on-click
+                movieTitle.push(title);
+                movieSynopsis.push(overview);
+            }
+
+            $('.carousel').carousel();
+        }
+
+        $(document.body).on("click", "#movie-1", function () {
+            $("#recommendation").empty();
+            // Hook into contentDiv
+            // Adds title and overview to DOM 
+            $("#recommendation").html("<h4>" + movieTitle[0] + "</h4><p>" + movieSynopsis[0] + "</p>")
+        });
+        $(document.body).on("click", "#movie-2", function () {
+            $("#recommendation").empty();
+            // Hook into contentDiv
+            // Adds title and overview to DOM 
+            $("#recommendation").html("<h4>" + movieTitle[1] + "</h4><p>" + movieSynopsis[1] + "</p>")
+        });
+        $(document.body).on("click", "#movie-3", function () {
+            $("#recommendation").empty();
+            // Hook into contentDiv
+            // Adds title and overview to DOM 
+            $("#recommendation").html("<h4>" + movieTitle[2] + "</h4><p>" + movieSynopsis[2] + "</p>")
+        });
+        resetGlobals();
+
+        $("#recommendation").on('click', function () {
+            $.getJSON("https://itunes.apple.com/search?term=" + tvTitle[i], function (result) {
+                if (result.results.length > 0) { //Runs if array returns a result
+                    if (result.results[i].kind === "feature-movie") {
+                        //displays results in the DOM 
+                        $('body').append(result.results[i].previewUrl + '<br>')
+
+                    }
+                }
+            })
+        });
+    });
+
+
+};
+
+// Reset global variables
+function resetGlobals() {
+    userRating = '',
+        userGenre = '',
+        tvQueryURL = '',
+        movieQueryURL = '';
+
+
+};
